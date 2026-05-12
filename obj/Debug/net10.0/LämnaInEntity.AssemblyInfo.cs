829105a9a7a9243343a6eda8f330533b757d8906
DYKWIAM <maassee88@gmail.com>
2026-05-12 07:16:13 +0200

    removed migrations

--- obj/Debug/net10.0/LämnaInEntity.AssemblyInfo.cs
+++ obj/Debug/net10.0/LämnaInEntity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LämnaInEntity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+115f1d1968bca83a8ddb9394a39b9257c2733f4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f054c9684ec0cc6e050e6ddb6b50669931b6e73")]
 [assembly: System.Reflection.AssemblyProductAttribute("LämnaInEntity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LämnaInEntity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
